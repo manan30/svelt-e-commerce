e8445a0e2b4ea9596dd54d133fbc2ef62230a93b
Manan Joshi <mananjoshi1995@gmail.com>
2021-01-24 23:21:59 -0600

    First edit

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,9 @@
 <script>
-  export let name;
+  // export let name;
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
+  <!-- <h1>Hello {name}!</h1> -->
   <p>
     Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
     how to build Svelte apps.
@@ -18,12 +18,12 @@
     margin: 0 auto;
   }
 
-  h1 {
+  /* h1 {
     color: #ff3e00;
     text-transform: uppercase;
     font-size: 4em;
     font-weight: 100;
-  }
+  } */
 
   @media (min-width: 640px) {
     main {
